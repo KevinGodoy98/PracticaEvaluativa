5ec52d76d7e765436e6f5d3aad7abb92710e9993
KevinGodoy98 <kgodoym@est.ups.edu.ec>
2021-05-19 08:59:05 -0500

    Fin 08:59

---
+++ app/src/main/java/ec/edu/ups/practicaevaluativa1/FragmentLogin.kt
@@ -0,0 +1,49 @@
+package ec.edu.ups.practicaevaluativa1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import ec.edu.ups.practicaevaluativa1.databinding.FragmentLoginBinding
+import java.util.ArrayList
+
+class FragmentLogin : Fragment() {
+
+    private lateinit var binding : FragmentLoginBinding
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.btnEnviar.setOnClickListener {
+            chequearDatos(it)
+        }
+        return binding.root
+    }
+    fun chequearDatos(view: View){
+        var text : String? = ""
+        context?.openFileInput("Registrar").use { fileInputStream ->
+            text = fileInputStream?.bufferedReader().use {
+                it?.readText()
+            }
+        }
+        val usuario = text?.split(";");
+        if((binding.nombre == usuario?.get(0) && binding.password == usuario?.get(2))
+            || (binding.nombre == "admin" && binding.password == "admin")){
+            view.findNavController().navigate(R.id.action_fragment_login_to_fragmentInicio)
+        }else
+            Toast.makeText(context, "Los datos son incorrectos...", Toast.LENGTH_SHORT).show()
+
+    }
+
+}
---
+++ app/src/main/java/ec/edu/ups/practicaevaluativa1/FragmentRegistro.kt
@@ -0,0 +1,39 @@
+package ec.edu.ups.practicaevaluativa1
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import ec.edu.ups.practicaevaluativa1.databinding.FragmentRegistroBinding
+
+class FragmentRegistro : Fragment() {
+    private lateinit var binding : FragmentRegistroBinding
+    //private var textAnterior : String = "";
+   // private var operacion : String = "";
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_registro, container, false)
+        binding.btnRegistrar.setOnClickListener {
+            guardarUsuario()
+            view?.findNavController()?.navigate(R.id.action_fragment_registro_to_fragment_login)
+        }
+        return binding.root
+    }
+    fun guardarUsuario(){
+        val filename = "Registrar"
+        val fileContent = binding.nombre+";"+binding.direccion+";"+binding.password
+        context?.openFileOutput(filename, Context.MODE_PRIVATE).use {
+            it?.write(fileContent.toByteArray())
+        }
+
+    }
+
+
+
+}
